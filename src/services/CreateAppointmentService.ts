e66d4b1eec9c161895a7f75863c3d484146f9118
Bruno Carvalho <brunocarvalho@MacBook-Air-de-Bruno.local>
2020-08-30 23:25:17 -0300

    appointment get and post

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,35 @@
+import { getCustomRepository } from 'typeorm';
+import { startOfHour } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+export default class CreateAppointmentService {
+  public async execute({ date, provider }: Request): Promise<Appointment> {
+    const appointmentRepository = getCustomRepository(AppointmentRepository);
+
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await appointmentRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = appointmentRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    await appointmentRepository.save(appointment);
+
+    return appointment;
+  }
+}
